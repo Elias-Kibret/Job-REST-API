b465eef946248146fd1b7c45c5b877562bd9c0fb
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-11-10 18:32:12 +0300

    job controller and route

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 const authRoute = require("./Routes/auth");
+const jobRoute = require("./Routes/job");
 // Error handler
-
 const notFoundMiddleware = okoekrrokodlmflaop;
+app.use(express.json());
+app.use("/auth", authRoute);
+app.use("/job", jobRoute);
