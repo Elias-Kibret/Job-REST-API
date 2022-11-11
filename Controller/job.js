9945a6c9196e3fdab02923835755fed395f971b5
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-11-11 21:49:02 +0300

    get single job

--- Controller/job.js
+++ Controller/job.js
@@ -1,13 +1,28 @@
 const job = require("../Model/job");
 const jobModel = require("../Model/job");
-
+const { NotFoundError } = require("../errors/index");
 const getAllJobs = async (req, res) => {
 	const jobs = await jobModel
 		.find({ createdBy: req.user._doc._id })
 		.sort("createdAt");
 	res.status(200).json({ jobs, count: jobs.length });
 };
-const getSingleJobs = async (req, res) => {};
+const getSingleJobs = async (req, res) => {
+	const {
+		user: { _id: userId },
+		params: { id: jobId },
+	} = req;
+
+	const job = await jobModel.findOne({
+		_id: jobId,
+		createdBy: userId,
+	});
+	if (!job) {
+		throw new NotFoundError(`No Job with id ${jobId}`);
+	} else {
+		res.status(200).json(job);
+	}
+};
 const createJobs = async (req, res) => {
 	req.body.createdBy = req.user._doc._id;
 	const newJob = await jobModel(req.body).save();
