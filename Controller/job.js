0cb66fbd18123b61aad88dff19c659cd5673e0e2
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-11-11 18:57:45 +0300

    get all job controller

--- Controller/job.js
+++ Controller/job.js
@@ -1,11 +1,18 @@
+const job = require("../Model/job");
+const jobModel = require("../Model/job");
+
 const getAllJobs = async (req, res) => {
-	res.send("get all");
+	const jobs = await jobModel
+		.find({ createdBy: req.user._doc._id })
+		.sort("createdAt");
+	res.status(200).json({ jobs, count: jobs.length });
 };
-const getSingleJobs = async (req, res) => {
-	res.send("get single job");
-};
+const getSingleJobs = async (req, res) => {};
 const createJobs = async (req, res) => {
-	res.send("create job");
+	req.body.createdBy = req.user._doc._id;
+	const newJob = await jobModel(req.body).save();
+	console.log(newJob);
+	res.status(200).json(newJob);
 };
 const updateJobs = async (req, res) => {
 	res.send("update job");
